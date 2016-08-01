3e3c0ae9964df12e0eefa44baf6e08185739e43c
Dongyu Zhai <Naffaell@dongyudemacbook.local>
2016-08-01 23:12:23 +0800

    Initial Commit

---
+++ Nimble NinjaTests/Nimble_NinjaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Nimble_NinjaTests.swift
+//  Nimble NinjaTests
+//
+//  Created by Dongyu Zhai on 1/8/16.
+//  Copyright © 2016 Dongyu Zhai. All rights reserved.
+//
+
+import XCTest
+@testable import Nimble_Ninja
+
+class Nimble_NinjaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
